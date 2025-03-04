f189aa471a7ce48ae5d5f700e430d0858116e28e
RamilHlebnikov <ramilhlebnikov66@gmail.com>
2025-03-04 22:39:21 +0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+document.getElementById("main-action-button2").onclick = function () {
+    document.getElementById("footer").scrollIntoView( {behavior: "smooth"});
+}
+
+document.getElementById("main-action-button").onclick = function () {
+    document.getElementById("Childhood").scrollIntoView( {behavior: "smooth"});
+}
+
+const links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView( {behavior: "smooth"});
+    }
+}
+
+const prices = document.getElementsByClassName("products-item-price");
+document.getElementById("change-currency").onclick = function (e) {
+    const currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 90;
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+
+}
